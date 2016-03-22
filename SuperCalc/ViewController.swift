a6aa026aada72165a623ff167620f4c71af670c7
Natnicha Thonket <proud.loligoth@gmail.com>
2016-03-22 19:55:16 +0700

    first commit

---
+++ SuperCalc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SuperCalc
+//
+//  Created by Natnicha Thonket on 3/21/2559 BE.
+//  Copyright © 2559 Natnicha Thonket. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var Shift_but: NSLayoutConstraint!
+    
+
+}
+
