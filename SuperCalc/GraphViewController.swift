322c6221dbbf7bf2f3eba2a67f1812e99b628ca6
Karunyapas Dangruan <ioscarhunter@gmail.com>
2016-05-22 04:58:02 +0700

    add graph customise

--- SuperCalc/GraphViewController.swift
+++ SuperCalc/GraphViewController.swift
@@ -11,24 +11,113 @@ import SwiftCharts
 
 class GraphViewController: UIViewController {
 
+    @IBOutlet weak var xMin: UITextField!
+    @IBOutlet weak var xMax: UITextField!
+    @IBOutlet weak var yMin: UITextField!
+    @IBOutlet weak var yMax: UITextField!
+    
     @IBOutlet weak var boundsetting: UIStackView!
     private var chart: Chart? // arc
     private var xbound:(min:Double,max:Double) = (-5,5)
     private var ybound:(min:Double,max:Double) = (-5,5)
     
+    @IBAction func xMinEdited(sender: AnyObject) {
+        if let Min = xMin.text where !xMin.text!.isEmpty {
+            if let Max = xMax.text where !xMax.text!.isEmpty{
+        
+                if(Double(Min) >= Double(Max)!){
+                    xMin.textColor = UIColor.redColor()
+                }
+                else{
+                    xMin.textColor = UIColor.blueColor()
+                    xMax.textColor = UIColor.blueColor()
+            
+                    xbound = (Double(Min)!,Double(Max)!)
+                    updategraph()
+                }
+            }
+        }
+    }
+    @IBAction func xMaxEdited(sender: AnyObject) {
+        if let Min = xMin.text where !xMin.text!.isEmpty {
+            if let Max = xMax.text where !xMax.text!.isEmpty{
+                if(Double(Max) <= Double(Min)){
+                xMin.textColor = UIColor.redColor()
+            }
+            else{
+                xMin.textColor = UIColor.blueColor()
+                xMax.textColor = UIColor.blueColor()
+                
+                xbound = (Double(Min)!,Double(Max)!)
+                updategraph()
+            }
+        }
+        }
+    }
+    @IBAction func yMinEdited(sender: AnyObject) {
+        if let Min = yMin.text where !yMin.text!.isEmpty {
+            if let Max = yMax.text where !yMax.text!.isEmpty{
+                
+                if(Double(Min) >= Double(Max)!){
+                    yMin.textColor = UIColor.redColor()
+                }
+                else{
+                    yMin.textColor = UIColor.blueColor()
+                    yMax.textColor = UIColor.blueColor()
+                    
+                    ybound = (Double(Min)!,Double(Max)!)
+                    updategraph()
+                }
+            }
+        }
+    }
+    @IBAction func yMaxEdited(sender: AnyObject) {
+        if let Min = yMin.text where !yMin.text!.isEmpty {
+            if let Max = yMax.text where !yMax.text!.isEmpty{
+                if(Double(Max) <= Double(Min)){
+                    yMin.textColor = UIColor.redColor()
+                }
+                else{
+                    yMin.textColor = UIColor.blueColor()
+                    yMax.textColor = UIColor.blueColor()
+                    
+                    ybound = (Double(Min)!,Double(Max)!)
+                    updategraph()
+                }
+            }
+        }
+    }
     override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation){
         
         // Reload Data here
-        drawgraph((-5,5), yAxisnum: (-5,5), chartPointsData: generateXY((-5,5),ybound: (-5,5)))
-        removeChart()
+        updategraph()
+        
         self.view.setNeedsDisplay()
         
     }
+    
+    private func updategraph(){
+        removeChart()
+        drawgraph(self.xbound, yAxisnum: self.ybound, chartPointsData: generateXY(self.xbound,ybound:self.ybound))
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        xMin.text = "\(xbound.min)"
+        xMax.text = "\(xbound.max)"
+        yMin.text = "\(ybound.min)"
+        yMax.text = "\(ybound.max)"
         
-        drawgraph((-5,5), yAxisnum: (-5,5), chartPointsData: generateXY((-5,5),ybound: (-5,5)))
+        xMin.textColor = UIColor.blueColor()
+        xMax.textColor = UIColor.blueColor()
+        yMin.textColor = UIColor.blueColor()
+        yMax.textColor = UIColor.blueColor()
         
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        drawgraph(xbound, yAxisnum: ybound, chartPointsData: generateXY(xbound,ybound:ybound))
     }
     
     private func generateXY(xbound:(min:Double,max:Double),ybound:(min:Double,max:Double))->[(Double,Double)]{
@@ -52,9 +141,9 @@ class GraphViewController: UIViewController {
         
         let chartPoints = chartPointsData.map{ChartPoint(x: ChartAxisValueDouble($0	.0, labelSettings: labelSettings), y: ChartAxisValueDouble($0.1))}
         
-        let xValues = (xAxisnum.0).stride(through: xAxisnum.1, by: ((xAxisnum.1-xAxisnum.0)/10)).map {ChartAxisValueFloat(CGFloat($0), labelSettings: labelSettings)}
+        let xValues = (xAxisnum.0).stride(through: xAxisnum.1, by: ceil((xAxisnum.1-xAxisnum.0)/10)).map {ChartAxisValueFloat(CGFloat($0), labelSettings: labelSettings)}
 
-        let yValues = (yAxisnum.0).stride(through: yAxisnum.1, by: ((yAxisnum.1-yAxisnum.0)/10)).map {ChartAxisValueFloat(CGFloat($0), labelSettings: labelSettings)}
+        let yValues = (yAxisnum.0).stride(through: yAxisnum.1, by: ceil((yAxisnum.1-yAxisnum.0)/10)).map {ChartAxisValueFloat(CGFloat($0), labelSettings: labelSettings)}
         
         let xModel = ChartAxisModel(axisValues: xValues, axisTitleLabel: ChartAxisLabel(text: "x", settings: labelSettings))
         let yModel = ChartAxisModel(axisValues: yValues, axisTitleLabel: ChartAxisLabel(text: "y", settings: labelSettings.defaultVertical()))
@@ -62,7 +151,7 @@ class GraphViewController: UIViewController {
         let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.size.height - 5
         let textcalheight = boundsetting.frame.size.height
         
-        let chartFrame = CGRectMake(-7, textcalheight+statusBarHeight, self.view.bounds.size.width, (self.view.bounds.size.height - textcalheight - statusBarHeight-80))
+        let chartFrame = CGRectMake(-7, textcalheight+statusBarHeight-7, self.view.frame.size.width, (self.view.frame.size.height - textcalheight - statusBarHeight+14))
         
         let coordsSpace = ChartCoordsSpaceLeftBottomSingleAxis(chartSettings: ExamplesDefaults.chartSettings, chartFrame: chartFrame, xModel: xModel, yModel: yModel)
         let (xAxis, yAxis, innerFrame) = (coordsSpace.xAxis, coordsSpace.yAxis, coordsSpace.chartInnerFrame)
@@ -70,9 +159,46 @@ class GraphViewController: UIViewController {
         let lineModel = ChartLineModel(chartPoints: chartPoints, lineColor: UIColor.redColor(), animDuration: 1, animDelay: 0)
         let chartPointsLineLayer = ChartPointsLineLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, lineModels: [lineModel])
         
+        // tracker layer
+//        let trackerLayerSettings = ChartPointsLineTrackerLayerSettings(thumbSize: Env.iPad ? 30 : 20, thumbCornerRadius: Env.iPad ? 16 : 10, thumbBorderWidth: Env.iPad ? 4 : 2, infoViewFont: ExamplesDefaults.fontWithSize(Env.iPad ? 26 : 16), infoViewSize: CGSizeMake(Env.iPad ? 400 : 160, Env.iPad ? 70 : 40), infoViewCornerRadius: Env.iPad ? 30 : 15)
+//        let chartPointsTrackerLayer = ChartPointsLineTrackerLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, chartPoints: chartPoints, lineColor: UIColor.blackColor(), animDuration: 1, animDelay: 2, settings: trackerLayerSettings)
         
-        let trackerLayerSettings = ChartPointsLineTrackerLayerSettings(thumbSize: Env.iPad ? 30 : 20, thumbCornerRadius: Env.iPad ? 16 : 10, thumbBorderWidth: Env.iPad ? 4 : 2, infoViewFont: ExamplesDefaults.fontWithSize(Env.iPad ? 26 : 16), infoViewSize: CGSizeMake(Env.iPad ? 400 : 160, Env.iPad ? 70 : 40), infoViewCornerRadius: Env.iPad ? 30 : 15)
-        let chartPointsTrackerLayer = ChartPointsLineTrackerLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, chartPoints: chartPoints, lineColor: UIColor.blackColor(), animDuration: 1, animDelay: 2, settings: trackerLayerSettings)
+        let showCoordsTextViewsGenerator = {(chartPointModel: ChartPointLayerModel, layer: ChartPointsLayer, chart: Chart) -> UIView? in
+            let (chartPoint, screenLoc) = (chartPointModel.chartPoint, chartPointModel.screenLoc)
+            let w: CGFloat = 70
+            let h: CGFloat = 30
+            
+            let text = "(\(chartPoint.x.text), \(chartPoint.y.text))"
+            let font = ExamplesDefaults.labelFont
+            let textSize = ChartUtils.textSize(text, font: font)
+            let x = min(screenLoc.x + 5, chart.bounds.width - textSize.width - 5)
+            let view = UIView(frame: CGRectMake(x, screenLoc.y - h, w, h))
+            let label = UILabel(frame: view.bounds)
+            label.text = "(\(chartPoint.x.text), \(chartPoint.y.text))"
+            label.font = ExamplesDefaults.labelFont
+            view.addSubview(label)
+            view.alpha = 0
+            UIView.animateWithDuration(0.2, delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
+                view.alpha = 1
+                }, completion: nil)
+            return view
+        }
+        
+        let showCoordsLinesLayer = ChartShowCoordsLinesLayer<ChartPoint>(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, chartPoints: chartPoints)
+        let showCoordsTextLayer = ChartPointsSingleViewLayer<ChartPoint, UIView>(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, chartPoints: chartPoints, viewGenerator: showCoordsTextViewsGenerator)
+        
+        let touchViewsGenerator = {(chartPointModel: ChartPointLayerModel, layer: ChartPointsLayer, chart: Chart) -> UIView? in
+            let (chartPoint, screenLoc) = (chartPointModel.chartPoint, chartPointModel.screenLoc)
+            let s: CGFloat = 30
+            let view = HandlingView(frame: CGRectMake(screenLoc.x - s/2, screenLoc.y - s/2, s, s))
+            view.touchHandler = {
+                showCoordsLinesLayer.showChartPointLines(chartPoint, chart: chart)
+                showCoordsTextLayer.showView(chartPoint: chartPoint, chart: chart)
+            }
+            return view
+        }
+        let touchLayer = ChartPointsViewsLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, chartPoints: chartPoints, viewGenerator: touchViewsGenerator)
+        
         
         let settings = ChartGuideLinesDottedLayerSettings(linesColor: UIColor.blackColor(), linesWidth: ExamplesDefaults.guidelinesWidth)
         let guidelinesLayer = ChartGuideLinesDottedLayer(xAxis: xAxis, yAxis: yAxis, innerFrame: innerFrame, settings: settings)
@@ -83,8 +209,10 @@ class GraphViewController: UIViewController {
                 xAxis,
                 yAxis,
                 guidelinesLayer,
+                showCoordsLinesLayer,
                 chartPointsLineLayer,
-                chartPointsTrackerLayer
+                showCoordsTextLayer,
+                touchLayer,
             ]
         )
         chart.view.tag = 100
