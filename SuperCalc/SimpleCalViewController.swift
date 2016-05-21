65f0e7bef029467a423914f494637195f76bd394
Natnicha Thonket <proud.loligoth@gmail.com>
2016-05-22 01:17:43 +0700

    Do not commit all files!!!!!!

--- SuperCalc/SimpleCalViewController.swift
+++ SuperCalc/SimpleCalViewController.swift
@@ -7,36 +7,40 @@
 //
 
 import UIKit
-// Basic math functions
-func add(a: Double, b: Double) -> Double {
-    let result = a + b
-    return result
-}
-func sub(a: Double, b: Double) -> Double {
-    let result = a - b
-    return result
-}
-func mul(a: Double, b: Double) -> Double {
-    let result = a * b
-    return result
-}
-func div(a: Double, b: Double) -> Double {
-    let result = a / b
-    return result
-}
-
-typealias Binop = (Double, Double) -> Double
-let ops: [String: Binop] = [ "+" : add, "-" : sub, "*" : mul, "/" : div ]
+//// Basic math functions
+//func add(a: Double, b: Double) -> Double {
+//    let result = a + b
+//    return result
+//}
+//func sub(a: Double, b: Double) -> Double {
+//    let result = a - b
+//    return result
+//}
+//func mul(a: Double, b: Double) -> Double {
+//    let result = a * b
+//    return result
+//}
+//func div(a: Double, b: Double) -> Double {
+//    let result = a / b
+//    return result
+//}
+//
+//typealias Binop = (Double, Double) -> Double
+//let ops: [String: Binop] = [ "+" : add, "-" : sub, "*" : mul, "÷" : div ]
 
 class SimpleCalViewController: UIViewController {
     
-    
     var accumulator: Double = 0.0 // Store the calculated value here
     var userInput = "" // User-entered digits
+    var currentInput = ""
     var lastOp = ""
+    var isNeg = false
     
     var numStack: [Double] = [] // Number stack
     var opStack: [String] = [] // Operator stack
+    
+    var calNumStack: [Double] = []
+    var calOpStack: [String] = []
     
     // Looks for a single character in a string.
     func hasIndex(stringToSearch str: String, characterToFind chr: Character) -> Bool {
@@ -50,44 +54,161 @@ class SimpleCalViewController: UIViewController {
     
     func handleInput(str: String) {
         print("new input \(userInput)")
-        if(str == "+" || str == "-" || str == "*" || str == "/") {
+        if str == "+" || str == "-" || str == "×" || str == "÷" {
+            if currentInput == "+" || currentInput == "-" || currentInput == "×" || currentInput == "÷" {
+                // clear last operation
+                userInput = String(userInput.characters.dropLast())
+                opStack.removeLast()
+            } else if userInput == "" {
+                isNeg = true
+            } else {
+                // add number
+                numStack.append(Double(currentInput)!)
+            }
             lastOp = str
+            currentInput = str
+            opStack.append(str)
+        } else if str == "" {
+            if currentInput == "+" || currentInput == "-" || currentInput == "×" || currentInput == "÷" {
+                currentInput = ""
+            }
+            numStack.append(Double(currentInput)!)
+        } else {
+            if currentInput == "+" || currentInput == "-" || currentInput == "×" || currentInput == "÷" {
+                currentInput = ""
+            }
+            currentInput += str
         }
-//        if str == "-" {
-//            if userInput.hasPrefix(str) {
-//                // Strip off the first character (a dash)
-//                userInput = userInput.substringFromIndex(userInput.startIndex.successor())
-//            } else {
-//                userInput = str + userInput
-//            }
-//        } else {
-//            userInput += str
-//        }
         userInput += str
         textlabel.text = userInput
     }
 
+    func doCalc() -> Double {
+        print("\n do calc")
+        print(calNumStack)
+        print(calOpStack)
+        
+        var op = 0
+        var ans = calNumStack[calNumStack.count-1]
+        for num in 1..<calNumStack.count {
+            let opRev = calOpStack.count - 1 - op
+            let numRev = calNumStack.count - 1 - num
+            print("op = \(calOpStack[opRev]), \(opRev), \(numRev)")
+            if calOpStack[opRev] == "+" {
+                ans = numStack[numRev] + ans
+            } else if calOpStack[opRev] == "-" {
+                ans = numStack[numRev] - ans
+            } else if calOpStack[opRev] == "×" {
+                ans = numStack[numRev] * ans
+            } else if calOpStack[opRev] == "÷" {
+                ans = numStack[numRev] / ans
+            } else {
+                print("no operation \(opStack[opRev])")
+            }
+            op += 1
+            print("ans = \(ans)")
+        }
+        return ans
+    }
     
     func displayAnswer() {
-        // If the value is an integer, don't show a decimal point
         print(userInput)
-        if userInput[userInput.endIndex.predecessor()] == lastOp[lastOp.endIndex.predecessor()] {
-            userInput = String(userInput.characters.dropLast())
+        print(numStack)
+        print(opStack)
+        if(numStack.count <= opStack.count) {
+            let diff = opStack.count - numStack.count
+            // put 0 at first until n nidex, n = diff(opStack,numStack)
+            for i in 0...diff {
+                numStack.insert(Double(0), atIndex: i)
+            }
+            print(numStack)
         }
-        let exp: NSExpression = NSExpression(format: userInput)
-        var result: Double = exp.expressionValueWithObject(nil, context: nil) as! Double
-        print(result)
-        var iAcc = Int(result)
-        if result - Double(iAcc) == 0 {
+        
+        // do calculate
+        var ans = 0.0
+        calNumStack.append(numStack[0])
+        var isRightPre = false
+        var op = 0
+        
+        for num in 1..<numStack.count {
+            print("num loop : \(num) -> \(opStack[op])")
+            if opStack[op] == "+" {
+                if isRightPre {
+                    ans = doCalc()
+                    calNumStack.removeAll()
+                    calOpStack.removeAll()
+                    if ans >= 0 {
+                        calNumStack.append(ans)
+                    } else {
+                        calNumStack.append(0)
+                        calNumStack.append(abs(ans))
+                        calOpStack.append("-")
+                    }
+                    isRightPre = false
+                }
+                calOpStack.append(opStack[op])
+                calNumStack.append(numStack[num])
+            } else if opStack[op] == "-" {
+                if isRightPre {
+                    ans = doCalc()
+                    calNumStack.removeAll()
+                    calOpStack.removeAll()
+                    if ans >= 0 {
+                        calNumStack.append(ans)
+                    } else {
+                        calNumStack.append(0)
+                        calNumStack.append(abs(ans))
+                        calOpStack.append("-")
+                    }
+                    isRightPre = false
+                }
+                calOpStack.append(opStack[op])
+                calNumStack.append(numStack[num])
+            } else if opStack[op] == "×" {
+                isRightPre = true
+                calOpStack.append(opStack[op])
+                calNumStack.append(numStack[num])
+            } else if opStack[op] == "÷" {
+                isRightPre = true
+                calOpStack.append(opStack[op])
+                calNumStack.append(numStack[num])
+            } else {
+                print("no operation \(opStack[op])")
+            }
+            op += 1
+        }
+        if isRightPre {
+            ans = doCalc()
+        } else {
+            print("\n do normal calc")
+            print(calNumStack)
+            print(calOpStack)
+            op = 0
+            ans = calNumStack[0]
+            for num in 1..<calNumStack.count {
+                if calOpStack[op] == "+" {
+                    ans = ans + calNumStack[num]
+                } else if calOpStack[op] == "-" {
+                    ans = ans - calNumStack[num]
+                } else {
+                    print("no operation \(calOpStack[op])")
+                }
+                op += 1
+                print("ans = \(ans)")
+            }
+        }
+        
+        // display output
+        let iAcc = Int(ans)
+        if ans - Double(iAcc) == 0 {
             resultlabel.text = String(iAcc)
         } else {
-            resultlabel.text = "\(result)"
+            resultlabel.text = "\(ans)"
         }
-        result = 0
-        iAcc = 0
     }
     
     func doEquals() {
+        handleInput("")
         print("ans of \(userInput)")
         if userInput == "" {
             return
@@ -141,10 +262,10 @@ class SimpleCalViewController: UIViewController {
          doEquals()
     }
     @IBAction func btn_dev(sender: AnyObject) {
-        handleInput("/")
+        handleInput("÷")
     }
     @IBAction func btn_mul(sender: AnyObject) {
-        handleInput("*")
+        handleInput("×")
     }
     @IBAction func btn_minus(sender: AnyObject) {
         handleInput("-")
@@ -154,8 +275,13 @@ class SimpleCalViewController: UIViewController {
     }
     @IBAction func btn_del(sender: AnyObject) {
         userInput = ""
+        currentInput = ""
+        numStack.removeAll()
+        opStack.removeAll()
+        calNumStack.removeAll()
+        calOpStack.removeAll()
         textlabel.text = ""
-        resultlabel.text = ""
+        resultlabel.text = nil
         print("reset ans \(resultlabel.text)")
     }
     override func viewDidLoad() {
